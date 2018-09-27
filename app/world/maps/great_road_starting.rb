dfed90e4a78297e895e40d9d46c4fc5dba533ae9
Koi_fish <pavels.gaimovics@gmail.com>
2018-09-27 20:07:59 +0300

    Modify great road starting map

--- app/world/maps/great_road_starting.rb
+++ app/world/maps/great_road_starting.rb
@@ -1,12 +1,21 @@
 class World::Maps::GreatRoadStarting < World::Maps::Base
   def draw
+    r = c(:road)
+    t = c(:tree)
+    b = c(:big_tree)
+    l = c(:light_tree)
+    g = u(:goblin)
+    x = u(:player)
+
     map =
       [
-        [c(:road), c(:road), c(:road),   c(:road), c(:road)],
-        [c(:road), c(:road), u(:goblin), c(:road), c(:road)],
-        [c(:road), c(:road), c(:road),   c(:road), c(:road)],
-        [c(:road), c(:road), u(:player), c(:road), c(:road)],
-        [c(:road), c(:road), c(:road),   c(:road), c(:road)],
+        [t, r, r, r, r, r, r, r, r, r, r, l, b],
+        [t, b, r, r, r, r, g, r, r, r, r, r, t],
+        [l, r, r, r, r, r, r, r, r, r, r, b, t],
+        [t, l, r, r, r, r, r, r, r, r, r, r, l],
+        [b, r, r, r, r, r, r, r, r, r, r, t, t],
+        [t, r, r, r, r, r, x, r, r, r, r, l, b],
+        [t, b, r, r, r, r, r, r, r, r, r, r, t],
       ]
   end
 end
