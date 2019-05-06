ae1737f8e1bdadfed0870ae7569af8c783dc6fb3
Koi_fish <pavels.gaimovics@gmail.com>
2019-05-06 11:46:10 +0300

    Return back cells collection changes

--- app/world/cells.rb
+++ app/world/cells.rb
@@ -1,22 +1,2 @@
-class World::Cells
-  extend Forwardable
-  include Singleton
-
-  def_delegators :@cells, :each, :clear
-
-  def initialize
-    @cells = []
-  end
-
-  def push(unit)
-    @cells.push(unit) unless @cells.include?(unit)
-  end
-
-  def to_s
-    @cells.map(&:name).compact.join(', ')
-  end
-
-  def descriable_cells
-    @cells.map()
-  end
+module World::Cells
 end
